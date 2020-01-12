efcf3a3f8aa4748ea4b83b439b40330388c127f8
Bryan Yunis <bryany@booj.com>
2020-01-11 21:25:40 -0700

    add rudimentary date countdown

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('HI!')
+
+
+const timeUntilGracesBirthday = () => {
+    const today = new Date();
+    const gracesBday = new Date('2/21/2020');
+    const milliseconds = gracesBday - today;
+    const millsInADay = 86400000;
+    const days = (milliseconds/millsInADay).toFixed(2)
+
+    return days;
+}
+
+document.getElementById('days').innerHTML = timeUntilGracesBirthday()
