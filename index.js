11e877003d62aec8f40a301881a8226694d4c95b
Bryan Yunis <bryany@booj.com>
2020-01-12 18:49:59 -0700

    add fireworks and isDay logic

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 
+const gracesBday = new Date(`1/12/${new Date().getFullYear()}`);
 
 const format = (time, unit) => {
     const roundedTime = parseInt(time);
     return roundedTime !== 1 ? `${roundedTime} ${unit}s` : `${roundedTime} ${unit}`
 }
+const isGracesBirthday = () => {
+    const today = new Date();
+    const millisecondsInADay = 86400000;
+    if ((gracesBday - today) < millisecondsInADay) {
+        return true;
+    }
+    return false;
+}
+
+const itsHerBirthday = () => {
+    document.getElementById('yes-or-no').innerHTML = 'Yes!';
+    document.getElementById('time-until').innerHTML = '🎉 Wish Her A Happy Birthday 🎉';
+}
 const getTimeUntilGracesBday = () => {
     const today = new Date();
-    const thisYear = today.getFullYear()
-    const gracesBday = new Date(`2/21/${thisYear}`);
-    const difference = gracesBday - today;
-    const days = difference / (24 * 3600 * 1000);
+    const differenceInMillis = gracesBday - today;
+    const days = differenceInMillis / (24 * 3600 * 1000);
     const hours = 24 * (days % 1);
     const minutes = 60 * (hours % 1);
     const seconds = 60 * (minutes % 1);
 
-    const humanReadableDiff = `${format(days, 'day')}
+    const humanReadableDiff = `
+                                ${format(days, 'day')}
                                  ${format(hours, 'hour')} 
                                  ${format(minutes, 'minute')} 
-                                 ${format(seconds, 'second')}`
+                                 ${format(seconds, 'second')}
+                                `
 
     document.getElementById('time').innerHTML = humanReadableDiff
 }
-getTimeUntilGracesBday()
 
-setInterval(getTimeUntilGracesBday, 1000)
+if (isGracesBirthday()) {
+    itsHerBirthday();
+} else {
+    getTimeUntilGracesBday();
+    setInterval(getTimeUntilGracesBday, 1000);
+}
+
