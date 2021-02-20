17a338ac2d9ab7d0480557fdcc544361a374a655
Bryan Yunis <bryany@booj.com>
2021-02-20 07:52:47 -0700

    Fix off-by-one error
    Who'd have thunk it?

--- index.js
+++ index.js
@@ -1,61 +1,68 @@
 const yearOfNextBDay = () => {
-    const thisYear = new Date().getFullYear();
-    const today = new Date();
-    const endOfGracesBirthday = new Date(`2/22/${thisYear}`);
-    if (today > endOfGracesBirthday) {
-        return new Date().getFullYear() + 1;
-    }
-    return new Date().getFullYear();
-    }
-    const gracesBday = new Date(`2/21/${yearOfNextBDay()}`);
-    
-    const format = (time, unit) => {
-    const roundedTime = parseInt(time);
-    return roundedTime !== 1 ? `${roundedTime} ${unit}s` : `${roundedTime} ${unit}`;
-}
+  const thisYear = new Date().getFullYear();
+  const today = new Date();
+  const endOfGracesBirthday = new Date(`2/22/${thisYear}`);
+  if (today > endOfGracesBirthday) {
+    return new Date().getFullYear() + 1;
+  }
+  return new Date().getFullYear();
+};
+const gracesBday = new Date(`2/21/${yearOfNextBDay()}`);
+
+const format = (time, unit) => {
+  const roundedTime = parseInt(time);
+  return roundedTime !== 1
+    ? `${roundedTime} ${unit}s`
+    : `${roundedTime} ${unit}`;
+};
 const isGracesBirthday = () => {
-    const today = new Date();
-    const millisecondsInADay = 86400000;
-    if ((gracesBday - today) < millisecondsInADay) {
-        return true;
-    }
-    return false;
-}
+  const today = new Date();
+  const millisecondsInADay = 86400000;
+  const gracesBday = new Date(`2/21/${yearOfNextBDay()}`);
+  console.log(today, gracesBday, today-gracesBday)
+  if ((today - gracesBday > 0) && (today - gracesBday < millisecondsInADay)) {
+    return true;
+  }
+  return false;
+};
 
 const itsHerBirthday = () => {
-    document.getElementById('yes-or-no').innerHTML = 'Yes!';
-    document.getElementById('time-until').innerHTML = '🎉 Wish Her a Happy Birthday 🎉';
-    document.getElementById('fireworks').style.display = 'inherit';
-}
+  document.getElementById("yes-or-no").innerHTML = "Yes!";
+  document.getElementById("time-until").innerHTML =
+    "🎉 Wish Her a Happy Birthday 🎉";
+  document.getElementById("fireworks").style.display = "inherit";
+};
 
 const getTimeUntilGracesBday = () => {
-    const birthdayGirl = isGracesBirthday();
+  const birthdayGirl = isGracesBirthday();
 
-    if (birthdayGirl) {
-        itsHerBirthday();
-    }
-    const today = new Date();
-    const differenceInMillis = gracesBday - today;
-    const days = differenceInMillis / (24 * 3600 * 1000);
-    const hours = 24 * (days % 1);
-    const minutes = 60 * (hours % 1);
-    const seconds = 60 * (minutes % 1);
+  if (birthdayGirl) {
+    itsHerBirthday();
+  }
+  const today = new Date();
+  const differenceInMillis = gracesBday - today;
+  const days = differenceInMillis / (24 * 3600 * 1000);
+  const hours = 24 * (days % 1);
+  const minutes = 60 * (hours % 1);
+  const seconds = 60 * (minutes % 1);
 
-    const humanReadableDiff = `${format(days, 'day')}, ${format(hours, 'hour')}, ${format(minutes, 'minute')}, ${format(seconds, 'second')}`
+  const humanReadableDiff = `${format(days, "day")}, ${format(
+    hours,
+    "hour"
+  )}, ${format(minutes, "minute")}, ${format(seconds, "second")}`;
 
-    if (!birthdayGirl) {
-        document.getElementById('time').innerHTML = humanReadableDiff;
-    }
-}
+  if (!birthdayGirl) {
+    document.getElementById("time").innerHTML = humanReadableDiff;
+  }
+};
 
 // run timer
 getTimeUntilGracesBday();
 
 setInterval(() => {
-    if (isGracesBirthday()) {
-        itsHerBirthday();
-    } else {
-        getTimeUntilGracesBday();
-    }
+  if (isGracesBirthday()) {
+    itsHerBirthday();
+  } else {
+    getTimeUntilGracesBday();
+  }
 }, 1000);
-
